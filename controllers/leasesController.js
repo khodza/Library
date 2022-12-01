8a6ad2f94401e5107248ce982984c8ae5a2e6db2
Izzattillakhodza Makhmudkhodzaev <khodzapro@gmail.com>
2022-12-01 23:37:24 +0500

    Edited adding leases

--- controllers/leasesController.js
+++ controllers/leasesController.js
@@ -1,11 +1,28 @@
 const handleFactory = require("../handlers/handleFactory");
 const Lease = require("../modules/leaseModule");
+const Book = require("../modules/bookModule");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllLeases = handleFactory.getAll(Lease);
 exports.getLease = handleFactory.getOne(Lease);
-exports.addLease = handleFactory.createOne(Lease);
+exports.addLease = catchAsync(async (req, res, next) => {
+  const book = await Book.findById(req.body.orderedBook);
+  if (!book) return next(new AppError("Bunday kitob bazada mavjud emas!", 404));
+  if (book.amount <= 0) {
+    return next(new AppError("Bu kitob kutubxonada qolmagan!", 404));
+  }
+  book.amount -= 1;
+  await book.save();
+
+  const doc = await Lease.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      doc,
+    },
+  });
+});
 exports.updateLease = handleFactory.updateOne(Lease);
 exports.deleteLease = catchAsync(async (req, res, next) => {
   const lease = await Lease.findById(req.params.id);
