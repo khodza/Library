f91c778a7c20e8f4478c4d346bc0d12485ff8b0c
Izzattillakhodza Makhmudkhodzaev <khodzapro@gmail.com>
2022-12-02 18:58:16 +0500

    auto count amount of books

--- modules/bookModule.js
+++ modules/bookModule.js
@@ -1,38 +1,45 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, `Kitob nomini kiriting`],
+const bookSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, `Kitob nomini kiriting`],
+    },
+    author: {
+      type: String,
+      required: [true, `Kitob avtorini kiriting`],
+    },
+    year: {
+      type: Number,
+    },
+    pages: {
+      type: Number,
+      required: [true, `Kitob betlari sonini kiriting`],
+    },
+    wb_code: {
+      type: String,
+    },
+    codes: {
+      type: [String],
+      required: [true, "Kitobning Seria raqamini kiriting!"],
+    },
+    get_from: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
   },
-  author: {
-    type: String,
-    required: [true, `Kitob avtorini kiriting`],
-  },
-  year: {
-    type: Number,
-  },
-  pages: {
-    type: Number,
-    required: [true, `Kitob betlari sonini kiriting`],
-  },
-  wb_code: {
-    type: String,
-  },
-  codes: {
-    type: [String],
-    required: [true, "Kitobning Seria raqamini kiriting!"],
-  },
-  get_from: {
-    type: String,
-  },
-  price: {
-    type: Number,
-  },
-  amount: {
-    type: Number,
-    default: 1,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+bookSchema.virtual("amount").get(function () {
+  console.log(this.codes);
+  return this.codes.length;
 });
 
 const Book = mongoose.model("Book", bookSchema);
