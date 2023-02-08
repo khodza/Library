df39dc2073c8b70ccc548efe9583b674ec280a84
Izzattillakhodza Makhmudkhodzaev <khodzapro@gmail.com>
2023-02-08 10:19:26 +0500

    protection

--- routes/leaseRoute.js
+++ routes/leaseRoute.js
@@ -4,7 +4,7 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-// router.use(authController.protect);
+router.use(authController.protect);
 router.route("/download").get(leasesController.downloadAllLeases);
 router.route("/history/download").get(leasesController.downloadAllHistory);
 
